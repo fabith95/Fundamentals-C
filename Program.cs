4864ce3360e3944e70c9cfff5e7ee268be65ba6b
fabith95 <fabith95@gmail.com>
2024-01-18 15:00:12 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Describe tu nombre");
+
+using System.Data;
+
+//char userSelection = 'a';
+
+//Console.WriteLine(userSelection);
+
+//char isDigit = char.IsDigit(userSelection);
+
+//bool upperVersion = char.ToUpper(userSelection);
+
+//bool isLetter = char.IsLetter(userSelection);
+
+//DataTime employee = new DataTime(2025, 03, 28);
+//DateTime today = DataSetDateTime.today;
+//DataTime twoDaysLater = someDateTime.AddDays(2);
+//DayOfWeek day = someDataTime.Dayofweek;
+//bool isDST = someDataTime.IsDaylightSavingTime();
+
+DateTime hireDate = new DateTime(2022, 03, 28, 14, 30, 0);
+Console.WriteLine(hireDate);
+
+
+DateTime exitDate = new DateTime(2025, 12, 11);
+//DateTime inalidDate = new DateTimeTime(2025, 15, 11);
+
+DateTime startDate = hireDate.AddDays(15);
+Console.WriteLine(startDate);
+
+
+DateTime CurrentDate = DateTime.Now;
+bool areWeInDst = CurrentDate.IsDaylightSavingTime();
+
+
+
+Console.ReadLine();
+
